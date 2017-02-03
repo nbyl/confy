20a5dc76488607f0653d6885ef893d2e11e0d036
Nicolas Byl <nico@nicolas-byl.eu>
2017-02-03 22:10:25 +0100

    finished speaker and editor

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,22 +1,34 @@
 'use strict';
 
 var myApp = angular.module('confy', ['ng-admin']);
+
+myApp.config(['RestangularProvider', function(RestangularProvider) {
+    RestangularProvider.addResponseInterceptor(function(data, operation, what) {
+        if (operation == 'getList') {
+            var resp =  data._embedded[what];
+            resp._links = data._links;
+            return resp;
+        }
+        return data;
+    });
+}]);
+
 // declare a function to run when the module bootstraps (during the 'config' phase)
 myApp.config(['NgAdminConfigurationProvider', function (nga) {
    var admin = nga.application('My First Admin')
          .baseApiUrl('/api/'); // main API endpoint
    var speaker = nga.entity('speakers');
-   speaker.listView().fields([
-       nga.field('surname').isDetailLink(true),
-       nga.field('firstName')
-   ]);
+   speaker.listView()
+      .fields([
+          nga.field('surname'),
+          nga.field('firstName')
+      ])
+      .listActions(['edit', 'delete']);
    speaker.creationView().fields([
        nga.field('surname'),
        nga.field('firstName')
    ]);
-   console.log(speaker.creationView().fields());
    speaker.editionView().fields(speaker.creationView().fields());
-   console.log(speaker.editionView().fields());
    admin.addEntity(speaker);
 
    nga.configure(admin);
