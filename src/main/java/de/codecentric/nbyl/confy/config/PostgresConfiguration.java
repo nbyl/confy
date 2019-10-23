e917e161959dafc8e3d867229afcf2bf66efa3be
Nicolas Byl <nico@nicolas-byl.eu>
2019-10-23 12:54:13 +0200

    add postgresconfig

---
+++ src/main/java/de/codecentric/nbyl/confy/config/PostgresConfiguration.java
@@ -0,0 +1,30 @@
+package de.codecentric.nbyl.confy.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class PostgresConfiguration {
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(dbUrl);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.addDataSourceProperty("ssl.mode", "disable");
+
+        return ds;
+    }
+}
